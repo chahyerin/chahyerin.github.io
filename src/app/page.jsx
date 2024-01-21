95e1f3b4429296ec5c8adb59320d82c194ea415a
junzero741 <junzero741@gmail.com>
2024-01-21 17:18:50 +0900

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,6 @@
-import Image from 'next/image'
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      0
-    </main>
-  )
+	return <main className='flex min-h-screen flex-col items-center justify-between p-24'>test</main>;
 }
